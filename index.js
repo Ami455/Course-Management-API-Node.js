efe40205ec29e2b53120d97f15a70b5381f5281b
Ami455 <ameenabaht00@gmail.com>
2025-05-09 10:41:22 +0300

    vercel

--- index.js
+++ index.js
@@ -1,50 +1,24 @@
-// const express = require("express");
-// const app = express();
-// const connection = require("./db/db.config");
-// const bodyParser= require("body-parser");
-// const port = 3500;
-// const courseRoutes = require("./routes/course.route")
-
-// app.use(express.json()); //parser
-// app.use("/api/courses", courseRoutes);
-// app.get("/", (req, res) => {
-//     res.send("API is running");
-//   });
-  
-// app.listen(port, async (error) => {
-//     connection()
-//         .then(() => { console.log(`MongoDB cennected successfully! port num ${port}`) }).
-//         catch((error) => {
-//             console.log("MongoDB connection failed ", error)
-//         });
-
-// })
-
 const express = require("express");
 const app = express();
 const connection = require("./db/db.config");
-const bodyParser = require("body-parser");
-const courseRoutes = require("./routes/course.route");
+const port = 3500;
+const courseRoutes = require("./routes/course.route")
 
-const port = process.env.PORT || 3500;
-
-app.use(express.json());
+app.use(express.json()); //parser
 app.use("/api/courses", courseRoutes);
-
 app.get("/", (req, res) => {
-  res.send("API is running");
+    res.send("API is running");
 });
 
-connection()
-  .then(() => {
-    console.log(" MongoDB connected successfully!");
 
-    // Start the server ONLY after DB is connected
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
+connection()
+    .then(() => {
+        console.log(`MongoDB cennected successfully! port num ${port}`)
+        // Start the server ONLY after DB is connected
+        app.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
+    }).catch((error) => {
+        console.log("MongoDB connection failed ", error);
+        process.exit(1); // Optional: stop app from running
     });
-  })
-  .catch((error) => {
-    console.log("MongoDB connection failed:", error.message);
-    process.exit(1); // Optional: stop app from running
-  });
